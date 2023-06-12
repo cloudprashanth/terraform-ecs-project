60aa6d08320f1205ac3dbc62ce038e05ae139d16
Prashanth <durgaprashanthg@gmail.com>
2023-06-12 13:17:45 +0530

    modules/cloudwatch/logs name of the logs modified

--- modules/cloudwatch/logs.tf
+++ modules/cloudwatch/logs.tf
@@ -1,9 +1,9 @@
 # Set up CloudWatch group and log stream and retain logs for 3 days
 resource "aws_cloudwatch_log_group" "frontend_log_group" {
-  name              = "/ecs/${var.frontend_service_name}"
+  name              = "/ecs/${local.project_name}-${local.env}-${var.frontend_service_name}"
   retention_in_days = 3
 
   tags = {
-    Name = "/ecs/${local.project_name}-${local.env}-frontend"
+    Name = "/ecs/${local.project_name}-${local.env}-${var.frontend_service_name}"
   }
 }
